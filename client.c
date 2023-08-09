c9079c569228fbe614da2ce9e19d26bb5fbdbe13
Milo Craun <miloc@vt.edu>
2023-08-09 13:28:17 -0400

    Linux Abstract Domain Socket
    Switched to using Linux abstract sockets for the communication.
    This is *not portable*.

--- client.c
+++ client.c
@@ -4,21 +4,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define BUFSIZE 1024
+
 int main()
 {
     int data_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     
     struct sockaddr_un addr;
     addr.sun_family = AF_UNIX;
-    strcpy(addr.sun_path, "/home/milo/.pet/sock\0");
-    
-    connect(data_socket, (struct sockaddr *)&addr, sizeof(struct sockaddr_un));
+    strcpy(addr.sun_path+1, "test\0");
+    addr.sun_path[0] = '\0';    
 
-    char buf[1024];
+    connect(data_socket, (struct sockaddr *)&addr, sizeof(addr.sun_family) + 6);
 
-    recv(data_socket, buf, 1024, 0);
+    printf("What is the command: ");
+    char send_buf[BUFSIZE];
+    char recv_buf[BUFSIZE];
+    fgets(send_buf, BUFSIZE, stdin);
+
+    send(data_socket, "info\0", 5, 0); 
+
+
+    recv(data_socket, recv_buf, BUFSIZE, 0);
     
-    printf("Got %s from server\n", buf);
+    printf("Got %s from server\n", recv_buf);
     close(data_socket);
     return 0;
 }
--- server.c
+++ server.c
@@ -6,15 +6,37 @@
 
 int main()
 {
-    int listen_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    /* Code for using regular socket */
+    //int listen_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    //
+    //struct sockaddr_storage client;
+    //struct sockaddr_un addr;
+    //addr.sun_family = AF_UNIX;
+    //char path[108] = "/home/milo/.pet/sock\0";
+    //strcpy(addr.sun_path, path);
+
+    //unlink(addr.sun_path);
+    //int ret = bind(listen_socket, (struct sockaddr*)&addr, sizeof(struct sockaddr_un));
+    //
+    //ret = listen(listen_socket, 20);
+    //int addr_size = sizeof(client); 
+    //int data_socket = accept(listen_socket, (struct sockaddr*)&client, &addr_size);
     
+    //char *msg = "Hello World";    
+    //int length = strlen(msg);
+    
+    //send(data_socket, msg, length, 0);
+
     struct sockaddr_storage client;
+
+    int listen_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     struct sockaddr_un addr;
     addr.sun_family = AF_UNIX;
-    char path[108] = "/home/milo/.pet/sock\0";
-    strcpy(addr.sun_path, path);
-
-    int ret = bind(listen_socket, (struct sockaddr*)&addr, sizeof(struct sockaddr_un));
+    char path[108] = "\0test\0";
+    addr.sun_path[0] = '\0';
+    strncpy(addr.sun_path+1, path+1, 5);
+    
+    int ret = bind(listen_socket, (struct sockaddr*)&addr, sizeof(addr.sun_family) + 6);
     
     ret = listen(listen_socket, 20);
     int addr_size = sizeof(client); 
@@ -22,8 +44,17 @@ int main()
     
     char *msg = "Hello World";    
     int length = strlen(msg);
+    
+    //send(data_socket, msg, length, 0);
 
-    send(data_socket, msg, length, 0);
+    char buf[1024];
+    recv(data_socket, buf, 1024, 0);
+    if (!strcmp(buf, "info")) {
+        send(data_socket, "hi\0", 3, 0);
+    }
+
+    shutdown(data_socket, SHUT_RDWR);
+    shutdown(listen_socket, SHUT_RDWR);
     close(data_socket);
     close(listen_socket);
     return 0;
